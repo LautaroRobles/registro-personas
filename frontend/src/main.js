be90bdb1ead8c3c178fa6bbcaddcd019b89847fc
LautaroRobles <lautarorobles_oro@hotmail.com>
2021-10-18 19:27:57 -0300

    Vistas listas
    Co-authored-by: tomygabutti <tomygabutti@gmail.com>
    Co-authored-by: josebarreto97 <joseb1997@hotmail.com>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './plugins/router'
+import store from './plugins/vuex'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container fluid fill-height class="primary d-flex justify-center align-center">
+        <v-card width="400">
+            <v-card-title>Iniciar sesion</v-card-title>
+            <v-card-actions class="px-4 pb-4">
+                <v-form class="flex">
+                    <v-text-field
+                        label="Username"
+                    ></v-text-field>
+                    <v-text-field
+                        label="Password"
+                        type="password"
+                    ></v-text-field>
+                    <v-btn block color="primary">Continuar</v-btn>
+                </v-form>
+            </v-card-actions>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
